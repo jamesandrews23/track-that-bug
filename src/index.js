3e0f5c3841e0fa659d2732e56eadfc167d2eb651
jamesandrews23 <james.andrews23@gmail.com>
2020-09-22 17:29:47 -0500

    Adds the dashboard page with logout

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import Main from './Main';
 import Login from './Login';
 import Register from "./Register";
+import Dashboard from "./Dashboard";
 
 if(document.getElementById('root')){
     ReactDom.render(<Main/>, document.getElementById('root'));
@@ -10,4 +11,6 @@ if(document.getElementById('root')){
     ReactDom.render(<Login/>, document.getElementById('login'));
 } else if (document.getElementById('register')){
     ReactDom.render(<Register/>, document.getElementById('register'));
+} else if (document.getElementById('dashboard')){
+    ReactDom.render(<Dashboard/>, document.getElementById('dashboard'));
 }
